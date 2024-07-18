eb9af58f1999d63401c40e80ccee12c3dcb454a8
abhishekalandikar <95140474+abhishekalandikar@users.noreply.github.com>
2024-07-18 17:28:25 +0530

    Add files via upload

---
+++ Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CreateWordSample.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Aadhar { get; set; }
+        public string PAN { get; set; }
+    }
+}
